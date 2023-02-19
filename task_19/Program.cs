2481333794f25b2b52f463ab783cf5d7f2144b91
Evgeniy <gmail.com>
2023-02-20 00:15:24 +0300

    task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿int number;
+string number_n;
+Console.Write("Введите число: number  ");
+number_n = Console.ReadLine();
+number = Convert.ToInt32(number_n);
+
+var fist = number/10000;
+var second = number/1000 % 10;
+var fourth = number/100 % 10;
+var fith = number % 10;
+
+if (fist == fith)
+{
+  if(second == fourth)
+  {
+
+  }
+  Console.Write("Введенное число является палиндроном");
+}
+else
+{
+  Console.Write("Введенное число не является палиндроном");
+}
+
+
